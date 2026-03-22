0a10de96b88d69684ceba6fbfd39e6cbbb6e36ab
piasalmiakk <piamia1@live.no>
2026-03-22 13:11:52 +0100

    fixed file path in example

--- tests/testthat/test-import.R
+++ tests/testthat/test-import.R
@@ -1,5 +1,12 @@
 test_that("import works", {
-  imported_data <- import_species_data("data-raw/nea_cod_2025.html","nea_cod")
+
+  file_path <- system.file(
+    "extdata",
+    "nea_cod_2025.html",
+    package = "StockAssessmentModels"
+  )
+
+  imported_data <- import_species_data(file_path,"nea_cod")
 
   expect_true(!is.na(imported_data)) #testing if removal of NAs worked
   expect_true("Year" %in% imported_data[1,]) #testing if header is the correct row
