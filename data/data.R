e2cc1405f6a9f7ffa5dd867ca9bf1d789bce93eb
piasalmiakk <piamia1@live.no>
2026-03-25 12:08:16 +0100

    fixing ignore files: I think I found my mistake

---
+++ data/data.R
@@ -0,0 +1,20 @@
+#' Combined Stock-Recruitment dataset
+#'
+#' A dataset containing SSB and Recruitment time series data for different
+#' species of Norwegian fish stock.
+#'
+#' @format A data frame with columns:
+#' \describe{
+#'   \item{SSB}{Spawning stock biomass}
+#'   \item{Recruitment}{Number of recruits}
+#'   \item{Year}{Year of observation}
+#'   \item{species}{Species name}
+#'   \item{Low_SSB}{Lower bound of SSB}
+#'   \item{High_SSB}{Upper bound of SSB}
+#'   \item{Low_R}{Lower bound of Recruitment}
+#'   \item{High_R}{Upper bound of Recruitment}
+#' }
+#' @source Generated from stockassessment.org HTML files
+#' @usage data(combined_dataset)
+#' @keywords datasets
+"combined_dataset"
